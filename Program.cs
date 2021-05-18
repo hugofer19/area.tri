36e5883c384e9334f9153e7fe4147efbbc841a6d
Hugo Ferreira <46359390+hugofer19@users.noreply.github.com>
2021-05-18 22:51:33 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace triangulo_base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area;
+            double basee;
+            double altura;
+
+            Console.Write("Qual o valor da base:");
+            basee = double.Parse(Console.ReadLine());
+
+            Console.Write("Qual o valor da altura:");
+            altura = double.Parse(Console.ReadLine());
+
+            area = basee * altura / 2;
+
+            Console.WriteLine("A área é " + area + ".");
+            
+        }
+    }
+}
